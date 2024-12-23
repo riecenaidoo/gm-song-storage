14ed84faab6fcb484697870eed2c4ea7d816a896
riece <riece@psybergate.co.za>
2024-12-23 11:47:57 +0200

    added PlaylistService init tests

--- src/test/java/com/bobo/storage/TestConfig.java
+++ src/test/java/com/bobo/storage/TestConfig.java
@@ -3,15 +3,24 @@ package com.bobo.storage;
 import com.bobo.storage.config.AppConfig;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
+import org.springframework.transaction.annotation.Transactional;
 /**
- * <code>@SpringBootTest(classes = AppConfig.class)</code> to use the same configuration as the application when testing.
- * <p>
- * <code>@ActiveProfiles</code> to use the <code>test</code> profile, which I believe is a default configuration from Spring.
- * I am using this so the tests use <code>application-test.properties</code>.
+ * <ol>
+ *   <li>
+ *     <code>@SpringBootTest(classes = AppConfig.class)</code> to use the same configuration as the application when testing.
+ *   </li>
+ *   <li>
+ *     <code>@ActiveProfiles</code> to use the <code>test</code> profile, which I believe is a default configuration from Spring.
+ *     I am using this so the tests use <code>application-test.properties</code>.
+ *   </li>
+ *   <li>
+ *     <code>@Transactional</code> is specified to configure tests to run within a transaction and be rolled back after the test is executed.
+ *   </li>
+ * </ol>
  */
 @SpringBootTest(classes = AppConfig.class)
 @ActiveProfiles("test")
+@Transactional
 public interface TestConfig {
 
 }
