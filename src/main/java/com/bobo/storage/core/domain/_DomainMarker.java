6f4ec00c89ac65f7cf55763bb1a54e29575a623d
riece <riece@psybergate.co.za>
2025-01-31 17:50:09 +0200

    reword marker annotation documentation for clarity

--- src/main/java/com/bobo/storage/core/domain/_DomainMarker.java
+++ src/main/java/com/bobo/storage/core/domain/_DomainMarker.java
@@ -3,8 +3,10 @@ package com.bobo.storage.core.domain;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 /**
+ * Sole purpose is to be referenced for annotation based scanning.
+ * <p>
  * Prefaced with <code>_</code> as I want to easily distinguish this class to make sure it is not used in
- * business logic. Its purpose is simply for annotation base scanning.
+ * business logic.
  *
  * @see EntityScan#basePackageClasses()
  */
--- src/main/java/com/bobo/storage/core/resource/_ResourceMarker.java
+++ src/main/java/com/bobo/storage/core/resource/_ResourceMarker.java
@@ -3,8 +3,10 @@ package com.bobo.storage.core.resource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
+ * Sole purpose is to be referenced for annotation based scanning.
+ * <p>
  * Prefaced with <code>_</code> as I want to easily distinguish this class to make sure it is not used in
- * business logic. Its purpose is simply for annotation base scanning.
+ * business logic.
  *
  * @see EnableJpaRepositories#basePackageClasses()
  */
