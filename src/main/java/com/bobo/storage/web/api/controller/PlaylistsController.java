def1cc36f115a5a6d433edcf3a64d0f7be7aed81
riece <riece@psybergate.co.za>
2025-02-05 20:34:03 +0200

    revert PlaylistsPort refactor
    * misunderstood Ports & Adapters
    * impl increases complexity by way of redirection, and a slim controller seems like an anti-pattern

--- src/main/java/com/bobo/storage/web/api/controller/PlaylistsController.java
+++ src/main/java/com/bobo/storage/web/api/controller/PlaylistsController.java
@@ -1,56 +1,75 @@
 package com.bobo.storage.web.api.controller;
 
-import com.bobo.storage.web.api.port.PlaylistsPort;
+import com.bobo.storage.core.resource.query.PlaylistQueryRepository;
+import com.bobo.storage.core.service.PlaylistService;
 import com.bobo.storage.core.domain.Playlist;
 import com.bobo.storage.core.domain.Song;
 import com.bobo.storage.web.api.request.PlaylistsCreateRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/playlists")
 public class PlaylistsController {
 
-  private final PlaylistsPort playlists;
+  private final PlaylistService service;
 
-  @Autowired
-  public PlaylistsController(PlaylistsPort playlists) {
-    this.playlists = playlists;
+  private final PlaylistQueryRepository query;
+
+  public PlaylistsController(PlaylistService service, PlaylistQueryRepository query) {
+    this.service = service;
+    this.query = query;
   }
 
   @PostMapping
   public int create(@RequestBody PlaylistsCreateRequest request) {
-    return playlists.create(request.name, request.songs).getId();
+    Playlist playlist = service.save(new Playlist(request.name, songsOf(request.songs)));
+    return playlist.getId();
   }
 
   @GetMapping("{id}")
   public Playlist get(@PathVariable int id) {
-    return playlists.findById(id);
+    return query.findById(id).orElseThrow(() -> new RuntimeException("404"));
   }
 
   @GetMapping("{id}/songs")
   public List<String> getSongs(@PathVariable int id) {
     return get(id).getSongs().stream()
-                        .map((Song::getUrl)).collect(Collectors.toList());
+                  .map((Song::getUrl)).collect(Collectors.toList());
   }
 
   @PatchMapping("{id}/songs")
   public void updateSongs(@PathVariable int id, @RequestBody Collection<String> songs) {
-    playlists.updateSongs(id, songs);
+    service.updateSongs(get(id), songsOf(songs));
   }
 
   @PutMapping("{id}/songs")
   public void replaceSongs(@PathVariable int id, @RequestBody Collection<String> songs) {
-    playlists.replaceSongs(id, songs);
+    service.addSongs(get(id), songsOf(songs));
   }
 
   @PutMapping("{id}/name")
   public void replaceName(@PathVariable int id, @RequestBody String name) {
-    playlists.replaceName(id, name);
+    service.updateName(get(id), name);
+  }
+
+  // ------ Helpers for Fluency ------
+
+  /**
+   * The <code>songs</code> parameters in this controller represent the <code>Song.url</code> field
+   * of the <code>Song</code> entity.
+   *
+   * @param urls to map to <code>Song</code>(s)
+   * @return distinct collection of <code>Song</code>(s) objects. They are not yet managed entities.
+   */
+  private Set<Song> songsOf(Collection<String> urls) {
+    return urls.stream()
+               .map(Song::new)
+               .collect(Collectors.toSet());
   }
 
 }
