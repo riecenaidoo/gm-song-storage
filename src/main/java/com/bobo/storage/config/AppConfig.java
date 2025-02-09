defbe392edc813faf1f40571c72a921796776d8e
riece <riece@psybergate.co.za>
2025-02-09 13:10:14 +0200

    added Cors mapping for local dev

--- src/main/java/com/bobo/storage/config/AppConfig.java
+++ src/main/java/com/bobo/storage/config/AppConfig.java
@@ -3,16 +3,32 @@ package com.bobo.storage.config;
 import com.bobo.storage.App;
 import com.bobo.storage.core.domain._DomainMarker;
 import com.bobo.storage.core.resource._ResourceMarker;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * <code>@EnableWebMvc</code> - see <code>WebMvcConfigurer</code>: Used to define cors mapping in <code>#addCorsMappings</code>.
+ */
 @SpringBootApplication(scanBasePackageClasses = App.class)
 @EntityScan(basePackageClasses = _DomainMarker.class)
 @EnableJpaRepositories(basePackageClasses = _ResourceMarker.class)
 @EnableTransactionManagement
-public class AppConfig extends SpringBootServletInitializer {
+@EnableWebMvc
+public class AppConfig extends SpringBootServletInitializer implements WebMvcConfigurer {
+
+  @Value("${app.client.url}")
+  private String clientUrl;
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/api/v1/**").allowedOrigins(clientUrl);
+  }
 
 }
