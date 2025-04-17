b8eba31f47e0b97de7be156c2edab9eb977e041c
riece <riece@psybergate.co.za>
2025-04-17 22:48:23 +0200

    verified ObjectMapper handling of Optionals
    Tests that Request(s) are mapped as I expect by the Jackson ObjectMapper,
    when using the (default) autoconfiguration provided by Spring.
    Specifically, the handling of Optionals.
    
    Two asides;
    
    1. By the naming of the test, I probably should define the contract
       (interface) for Requests.
    2. I should look into test tagging so I can mark these types of
       exploratory/confirmation/library integration tests appropriately.
       I do not want them to run on every build.

---
+++ web/src/test/java/com/bobo/storage/web/api/request/RequestIT.java
@@ -0,0 +1,58 @@
+package com.bobo.storage.web.api.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Tests that {@code Request(s)} are mapped as I expect by the Jackson {@link ObjectMapper},
+ * when using the (default) autoconfiguration provided by Spring.
+ */
+@ContextConfiguration(classes = {JacksonAutoConfiguration.class})
+@ExtendWith(SpringExtension.class)
+class RequestIT {
+
+  private final ObjectMapper mapper;
+
+  @Autowired
+  RequestIT(ObjectMapper mapper) {
+    this.mapper = mapper;
+  }
+
+  /**
+   * @see Jdk8Module
+   */
+  @Nested
+  class Optionals {
+
+    @Test
+    void absentProperty() {
+      Map<String, String> payload = Map.of("key", "value");
+      RequestWithOptional request = mapper.convertValue(payload, RequestWithOptional.class);
+      Assertions.assertFalse(request.optionalProperty().isPresent());
+    }
+
+    @Test
+    void presentProperty() {
+      Map<String, String> payload = Map.of("optionalProperty", "value");
+      RequestWithOptional request = mapper.convertValue(payload, RequestWithOptional.class);
+      Assertions.assertTrue(request.optionalProperty().isPresent());
+    }
+
+    public record RequestWithOptional(Optional<String> optionalProperty, String requiredProperty) {
+
+    }
+
+  }
+
+}
