a1d48dab4b6a0de49378bcab168730bed5c02fe0
riece <riece@psybergate.co.za>
2025-05-30 00:00:31 +0200

    mv OEmbedResponse ->  Provider
    This Record serves as a DTO to map the Response from OEmbed. Since we
    are keeping the concerns of dealing with OEmbed localised to Provider,
    we do not need to expose this Record publically.
    
    [revisions]
    * OEmbedResponse implements Consumer<Song>. This is a similar idea to
      what I've done with PatchRequest<T>. The Response object is
      responsible for translating its data onto the Song object. If the
      OEmbed specification for Responses were to change, the change in code
      should be localised to the Response object.

--- oembed/src/main/java/com/bobo/storage/oembed/Provider.java
+++ oembed/src/main/java/com/bobo/storage/oembed/Provider.java
@@ -1,6 +1,7 @@
 package com.bobo.storage.oembed;
 
 import com.bobo.storage.core.domain.Song;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.ClientResponse;
@@ -13,6 +14,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 /**
  * The source of an embeddable form of content that adheres to the {@code oEmbed} specification.
@@ -81,16 +83,14 @@ public enum Provider {
     for (Provider provider : values()) {
       Optional<OEmbedResponse> metadata = provider.queryProvider(url, webClient);
       if (metadata.isPresent()) {
-        metadata.get().title().ifPresent(song::setTitle);
-        metadata.get().authorName().ifPresent(song::setArtist);
-        metadata.get().thumbnailUrl().ifPresent(song::setThumbnailUrl);
+        metadata.get().accept(song);
         return true;
       }
     }
     return false;
   }
 
-  public Optional<OEmbedResponse> queryProvider(URL url, WebClient webClient) {
+  private Optional<OEmbedResponse> queryProvider(URL url, WebClient webClient) {
     URL query = getQuery(url);
     URI queryURI;
     try {
@@ -115,6 +115,55 @@ public enum Provider {
    */
   private static Mono<OEmbedResponse> toMono(ClientResponse response) {
     return (response.statusCode().equals(HttpStatus.OK)) ? response.bodyToMono(OEmbedResponse.class) : Mono.empty();
+  }
+
+  /**
+   * @param type            The resource type.
+   *                        For this use case we only care about validating the existence the content,
+   *                        and any metadata about it. We may also forward the provider information for another consumer to retrieve
+   *                        the embedded content.
+   * @param version         The oEmbed version number. This must be {@code 1.0}.
+   * @param title           A text title, describing the resource.
+   * @param authorName      The name of the author/owner of the resource.
+   * @param authorUrl       A URL for the author/owner of the resource.
+   * @param providerName    The name of the resource provider.
+   * @param providerUrl     The url of the resource provider.
+   * @param cacheAge        The suggested cache lifetime for this resource, in seconds.
+   *                        Consumers may choose to use this value or not.
+   * @param thumbnailUrl    A URL to a thumbnail image representing the resource.
+   *                        The thumbnail must respect any {@code maxwidth} and {@code maxheight} parameters.
+   *                        If this parameter is present, {@code thumbnail_width} and {@code thumbnail_height} must also be present.
+   * @param thumbnailHeight The width of the optional thumbnail.
+   *                        If this parameter is present, {@code thumbnail_url} and {@code thumbnail_height} must also be present.
+   * @param thumbnailWidth  The height of the optional thumbnail.
+   *                        If this parameter is present, {@code thumbnail_url} and {@code thumbnail_width} must also be present.
+   * @see <a href="https://oembed.com/#section2.3">2.3.4. Response Parameters</a>
+   */
+  private record OEmbedResponse(
+          String type,
+          String version,
+          Optional<String> title,
+          @JsonProperty("author_name") Optional<String> authorName,
+          @JsonProperty("author_url") Optional<String> authorUrl,
+          @JsonProperty("provider_name") Optional<String> providerName,
+          @JsonProperty("provider_url") Optional<String> providerUrl,
+          @JsonProperty("cache_age") Optional<String> cacheAge,
+          @JsonProperty("thumbnail_url") Optional<String> thumbnailUrl,
+          @JsonProperty("thumbnail_height") Optional<String> thumbnailHeight,
+          @JsonProperty("thumbnail_width") Optional<String> thumbnailWidth
+  ) implements Consumer<Song> {
+
+    /**
+     * Accepts a Song to apply the metadata in the response to.
+     *
+     * @param song to apply the metadata to.
+     */
+    public void accept(Song song) {
+      title.ifPresent(song::setTitle);
+      authorName.ifPresent(song::setArtist);
+      thumbnailUrl.ifPresent(song::setThumbnailUrl);
+    }
+
   }
 
 }
