be4e5eb69bedd5f622ae9bbf381169c0afc860a6
riecenaidoo <riecenaidoo@outlook.com>
2025-08-24 13:06:35 +0200

    [housekeeping] pre-commit formatting
    - separated into three distinct checks (Staging, Format, Unittest).
    - hide actual command output and display only the issues that need
      resolution in red.
    - listed hints (post-failure) at the end in cyan.

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,53 +1,76 @@
 #!/bin/sh
-# Note: Run from repository root, not the ./scripts dir.
+
+printf "
+----------------------------
+Pre-Commit Check(s):
+----------------------------
+"
 
 # Use the MVN wrapper if available
 MVN="./mvnw"
 [ -x "$MVN" ] || MVN="mvn"
+# ========================================
+# Staging Check
+# ========================================
+printf "[\033[0;33m%s\033[0m] Checking... " "Staging"
+if ! git diff --quiet; then
+  printf '\033[0;31m%s\033[0m' "Failed!"
+  printf '%s\n' "	- The working directory has unstaged file changes."
+  printf '\n\033[0;31m'
+  git diff --shortstat
+  printf '\033[0m\n'
+  printf '%s\n' "This may lead to a false positive during pre-commit validation.
+Checks may pass because of changes present in your local repository that will not be included in the commit.
 
+Stash or stage the changes."
+  printf '\nHint:\t\033[0;36m%s\033[0m' "git add ." "git stash --keep-index"
+  exit 1
+fi
+printf '\033[0;32m%s\033[0m\n' "Passed."
+# ========================================
+# Formatting Check
+# ========================================
+printf "[\033[0;33m%s\033[0m] Checking... " "Format"
+# ... 2>&1 > /dev/null should work, but Maven [ERROR] is just formatted text on STDOUT
+STDOUT=$($MVN spotless:check)
+EXIT_CODE=$?
+if [ "$EXIT_CODE" -ne 0 ]; then
+	printf '\033[0;31m%s\033[0m' "Failed!"
+	printf '%s\n' "	- There are files with dirty format. Run formatting and restage."
+	printf '\n\033[0;31m'
+	printf '%s\n' "$STDOUT" | grep "\[ERROR\]"
+	printf '\033[0m\n'
+
+	printf 'Hint:\t\033[0;36m%s\033[0m\n' "mvn spotless:apply" "make format"
+  exit 1
+fi
+printf '\033[0;32m%s\033[0m\n' "Passed."
+# ========================================
+# Unittest Check
+# ========================================
+staged_src_files=$(git diff --cached --name-only | grep '\.java$')
+if [ -n "$staged_src_files" ]; then
+  printf "[\033[0;33m%s\033[0m] Checking... " "Unittest"
+  STDOUT=$($MVN test)
+  EXIT_CODE=$?
+  if [ "$EXIT_CODE" -ne 0 ]; then
+    printf '\033[0;31m%s\033[0m' "Failed!"
+    printf '%s\n' " - There are unittest failures. Resolve and restage."
+    printf '\n\033[0;31m'
+    printf '%s\n' "$STDOUT" | grep "\[ERROR\]"
+    printf '\033[0m\n'
+    exit 1
+  fi
+  printf '\033[0;32m%s\033[0m\n' "Passed."
+fi
+# ========================================
 # ANSI Color Escape Codes
+# ========================================
 # YELLOW='\033[0;33m'
 # RED='\033[0;31m'
 # GREEN='\033[0;32m'
+# CYAN='\033[0;36m'
+# BLUE='\033[0;34m'
 # NONE='\033[0m'
-
-unstaged_source_files=$(git diff --name-only | grep '\.java$')
-if [ -n "$unstaged_source_files" ]; then
-  printf '\n\n\033[0;31m%s\033[0m\n\n' "[Halted] Unstaged Changes!
-
-  The working directory has unstaged source file changes.
-  This may lead to a false positive during pre-commit validation.
-  Test cases may pass because of changes present in your local repository that will not be included in the commit.
-
-  Stage the changes, or stash them."
-  exit 1
-fi
-
-printf  '\n\n\033[0;33m%s\033[0m\n' "Checking formatting..."
-if ! $MVN spotless:check; then
-  printf '\n\n\033[0;31m%s\033[0m\n\n' "[Dirty] Resolve and restage files.
-
-  (Hint: mvn spotless:apply)
-  "
-  exit 1
-fi
-printf  '\n\n\033[0;32m%s\033[0m\n\n' "[Clean] Lint OK."
-
-staged_src_files=$(git diff --cached --name-only | grep '\.java$')
-if [ -n "$staged_src_files" ]; then
-  printf  '\n\n\033[0;33m%s\033[0m\n' "Running unit tests..."
-  if ! $MVN clean test; then
-    printf '\n\n\033[0;31m%s\033[0m\n\n' "[Fail] Unit test(s) failed!"
-    exit 1
-  fi
-else
-  printf  '\n\n\033[0;33m%s\033[0m\n' "No staged source file changed. Skipped unit tests."
-fi
-printf  '\n\n\033[0;32m%s\033[0m\n\n' "[OK] Looks good to me.
-
-(Hint: --no-verify for quick amending)"
-
-unstaged_files=$(git diff --name-only)
-if [ -n "$unstaged_files" ]; then
-  (printf '%s\n\n' "Unstaged Changes: (Hint: Q to exit)"; printf '%s\n' "$unstaged_files") | less
-fi
+# ========================================
+printf '%s\n' "----------------------------"
