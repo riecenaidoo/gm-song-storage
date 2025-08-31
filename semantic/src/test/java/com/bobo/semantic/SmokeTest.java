1f79c9798c57ef55d627c403896b1b2d01c2e49d
riecenaidoo <riecenaidoo@outlook.com>
2025-08-31 11:38:15 +0200

    added JUnit.Tag "smoke"
    We can leverage tagging to run a slice of tests. This changes updates
    the pre-commit to run SmokeTests rather than UnitTests, but this may
    need to be changed.
    
    Updates the Makefile to provide help information in "()" blue.

--- semantic/src/test/java/com/bobo/semantic/SmokeTest.java
+++ semantic/src/test/java/com/bobo/semantic/SmokeTest.java
@@ -2,23 +2,45 @@ package com.bobo.semantic;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 
 /**
  * A quick test that can be run to confirm the basic stability of the system.
  *
- * <p>We would typically run these after a build ({@code mvn compile}) so that we can fail fast
- * before attempting to run more exhaustive tests.
+ * <p>Typically run after a build ({@code mvn compile}) to fail fast before running more exhaustive
+ * tests.
  *
- * <p>An example can be something as simple as confirming the application can still boot-up. For a
- * dependency injected application, this could be confirming the application context can load
- * without an issue.
+ * <p>Examples include confirming the application can boot up successfully, or the application
+ * context can load without issues in a dependency-injected environment.
  *
- * @implSpec Suffix with {@code Test}, even although they may involve several systems, they are not
- *     considered an {@link IntegrationTest}. They are intended to be run as part of the initial
- *     build-test cycle ({@code mvn test}, see also Maven Surefire plugin), but are also not
- *     considered a {@link UnitTest}.
+ * @implSpec Suffix test class names with {@code Test}, because they should be run during regular
+ *     test cycles ({@code mvn test}), but note that they are not considered {@link UnitTest}.
+ *     Furthermore, although these tests may involve several systems, they are not considered {@link
+ *     IntegrationTest}.
  */
 @Documented
 @Target(ElementType.TYPE)
-public @interface SmokeTest {}
+@Retention(RetentionPolicy.RUNTIME)
+@Tag(SmokeTest.TAG)
+public @interface SmokeTest {
+
+	/**
+	 * The {@link Tag} value to use for {@link SmokeTest}.
+	 *
+	 * <p>Tagging via JUnit enables running a slice of tests via Maven.
+	 *
+	 * <pre>{@code
+	 * mvn test -Dgroups="smoke"
+	 * }</pre>
+	 *
+	 * @see Tags
+	 * @apiNote Annotating a test class with {@link SmokeTest} will automatically apply this tag. This
+	 *     {@code TAG} constant is available for special cases where you may want to refer to it
+	 *     directly.
+	 */
+	String TAG = "smoke";
+}
